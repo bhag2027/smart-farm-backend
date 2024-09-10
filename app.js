13fd8843f9e0df4ff317d82820bba9847d7494ea
bhag2027 <bhagyazz2027@gmail.com>
2024-09-10 18:55:33 +0530

    ok

--- app.js
+++ app.js
@@ -9,6 +9,37 @@ app.use(express.json())
 app.use(cors())
 
 mongoose.connect("mongodb+srv://bhagya:bhagya20@cluster0.gszky.mongodb.net/smartfarmdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/signin",async(req,res)=>{
+    let input=req.body
+    let result=usermodel.find({ email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0) {
+                const passwordvalidator=bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordvalidator) {
+                    jwt.sign({email:req.body.email},"farmapp",{expiresIn:"1d"},
+                    (error,token)=>{
+                   if (error) {
+                    res.json({"status":"error","error":error})
+                   } 
+                   else {
+                    res.json({"status":"success","token":token,"userid":items[0]._id})
+                   }
+                    })
+                } else {
+                    res.json({"status":"invalid password"})
+                    
+                }
+            } else {
+                res.json({"status":"invalid email"})
+                
+            }
+        }
+    ).catch()
+})
+
+
+
 app.post("/signup",async(req,res)=>{
     let input=req.body
     let hashedpassword=bcrypt.hashSync(req.body. password,10)
